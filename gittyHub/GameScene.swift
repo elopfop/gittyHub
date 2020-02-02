df08ebeed572334c948881ba8741480ef9b126fe
Eli Forman <s013941@students.lmsd.org>
2020-02-02 10:12:10 -0500

    deleted player2

--- gittyHub/GameScene.swift
+++ gittyHub/GameScene.swift
@@ -20,11 +20,7 @@ class GameScene: SKScene {
         player1.zPosition = 1.0
         addChild(player1)
         
-        let player2 = SKSpriteNode(imageNamed: "player2")
-        player2.setScale(0.5 )
-        player2.position = CGPoint(x: 0, y: 100)
-        player2.zPosition = 1.0
-        addChild(player2)
+        
         }
     
     
