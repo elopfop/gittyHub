167783b7984a8ddd7ce6f0ef682e30cbb77beecc
Eli Forman <s013941@students.lmsd.org>
2020-02-02 10:13:42 -0500

    added player2

--- gittyHub/GameScene.swift
+++ gittyHub/GameScene.swift
@@ -21,7 +21,7 @@ class GameScene: SKScene {
         addChild(player1)
         
         let player2 = SKSpriteNode(imageNamed: "player2")
-        player2.setScale(0.5 )
+        player2.setScale(0.4 )
         player2.position = CGPoint(x: 0, y: 100)
         player2.zPosition = 1.0
         addChild(player2)
